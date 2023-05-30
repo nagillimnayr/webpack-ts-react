a25ce281fab07bc2b134985e5ad676323bab7b7a
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-30 07:26:32 -0700

    feat: add App component to main function

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,21 @@
 import './style/style.css';
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
 const main = () => {
   const app = document.createElement('div');
   app.id = 'app';
   document.body.appendChild(app);
+
+  const root = ReactDOM.createRoot(
+    document.getElementById('app') as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 };
 window.addEventListener('load', main);
