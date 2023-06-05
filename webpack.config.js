13e8508d51e71119f70401f36390da3fd3603ed9
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-04 22:13:46 -0700

    fix: correct file extension in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/main.ts',
+    app: './src/main.tsx',
   },
   devtool: 'inline-source-map',
   devServer: {
